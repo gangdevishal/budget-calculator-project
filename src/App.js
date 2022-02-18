7c27a6ac787095fa669fc2c79d12ee7a901a3ce9
vishal gangde <gangdevishal222@gmail.com>
2022-02-18 13:58:40 +0530

    set up budget calculator project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Budget Calculator</h1>
+      <button className='btn btn-danger'>Click Here</button>
+    </div>
+  );
+}
+
+export default App;
